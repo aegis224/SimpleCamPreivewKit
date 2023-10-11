8df860e9474c2aa0d3992f44cc2b306b48709a37
aegis224 <aegis224@tutamail.com>
2023-10-10 18:29:10 -0700

    🌱 added Binary XCFramework

---
+++ CamcoderKit.xcframework/ios-arm64/CamcoderKit.framework/Headers/CamcoderKit.h
@@ -0,0 +1,18 @@
+//
+//  CamcoderKit.h
+//  CamcoderKit
+//
+//  Created by Preston Kim on 5/24/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CamcoderKit.
+FOUNDATION_EXPORT double CamcoderKitVersionNumber;
+
+//! Project version string for CamcoderKit.
+FOUNDATION_EXPORT const unsigned char CamcoderKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CamcoderKit/PublicHeader.h>
+
+
